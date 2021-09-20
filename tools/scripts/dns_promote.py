1efac91aac20c5ece2883f45f68e1b3affb2ded9
esteban <devlabmgmt@gmail.com>
2021-09-19 22:48:08 -0400

    ansible and dns promote script

--- tools/scripts/dns_promote.py
+++ tools/scripts/dns_promote.py
@@ -1,23 +1,66 @@
+from logging import error
 import boto3
 import argparse
 
+def route53():
+    
+    pass
+
+
 def main():
+    client = boto3.client('route53')
     parser = argparse.ArgumentParser(description="cli tool for challenege")
     parser.add_argument("--env", help="environment loadbalancer is in")
     parser.add_argument("--loadbalancer-name", help="loadbalancer name")
-
-
-
     args = parser.parse_args()
+    host_id = "clouddatadynamics.com"
+    print("starting")
+    paginator = client.get_paginator('list_resource_record_sets')
+    source_zone_records = paginator.paginate(HostedZoneId=host_id)
+    for record_set in source_zone_records:
+        print(source_zone_records)
+        for record in record_set['ResourceRecordSets']:
+            if record['ResourceRecords'][0]['Value'] == args.env:
+                try:
+                    change_res = client.change_resource_record_sets(
+                        HostedZoneId=host_id,
+                        ChangeBatch={
+                            'Changes' : [ 
+                            {
+                                'Action' : 'DELETE',
+                                'ResourceRecordSet' :{
+                                    'Name' : record['Name'],
+                                    'Type' : 'CNAME',
+                                    'TTL' : 300,
+                                    'ResourceRecords' : [
+                                        {
+                                            'Value' : record['ResourceRecords'][0]['Value']
+                                        },
+                                    ]
+                                }
+                            },
+                            {
+                                'Action' : 'UPSERT',
+                                'ResourceRecordSet' : {
+                                    'Name' : record['Name'],
+                                    'Type' : 'CNAME',
+                                    'TTL' : 300,
+                                    'ResourceRecords' : [
+                                        {
+                                            'Value' : args.loadbalancer_name
+                                        }
+                                    ]
 
-    if args.env == 'dev':
-        pass
-    elif args.env == 'stage':
-        pass
-    elif args.env == 'prod':
-        pass
-    else:
-        print("no correct env specified")
+                                }
+
+                            }
+                            ]
+                        }
+                    )
+                except Exception as err:
+                    print(err)
+
+
 
 
 if __name__ == '__main__':
